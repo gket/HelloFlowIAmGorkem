bc9f91eefb98d89b9fceae8f15dcb504e9630d42
Görkem Kethüda <gorkemkethuda@gmail.com>
2020-12-30 00:55:35 +0300

    [fix] Fix Github ghost user bug

--- app/src/main/java/com/gk/helloflowiamgorkem/utils/Utils.kt
+++ app/src/main/java/com/gk/helloflowiamgorkem/utils/Utils.kt
@@ -1,6 +1,7 @@
 package com.gk.helloflowiamgorkem.utils
 
 object Utils {
+    const val baseUrl = ""
     const val apiKey = "v8GI-i7eZcVi5JvZthXGl885VLvICu2tls6NOtf2-gM"
     const val secretKey = "E6_uP_hl0uiqRhI4sMt6ttB1lBKVTkswO-aqolUtGCU"
 }
