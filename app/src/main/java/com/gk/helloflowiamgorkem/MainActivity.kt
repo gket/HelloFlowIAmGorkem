e4a1e86b9a3660472189ba99dd57dd175ab19ad5
Görkem Kethüda <gorkemkethuda@gmail.com>
2020-12-30 08:58:07 +0300

    Created app navigation structure

--- app/src/main/java/com/gk/helloflowiamgorkem/MainActivity.kt
+++ app/src/main/java/com/gk/helloflowiamgorkem/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.gk.helloflowiamgorkem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.gk.helloflowiamgorkem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setContentView(view)
     }
 }
